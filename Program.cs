145758b68abdea9a7b07fc9d9103ab6f2ab30fb8
BentoMaranhao <bentomaranhao@gmail.com>
2022-10-18 20:44:41 -0300

    as

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace A{
+    class aka{
+        private static void Main(string[] args){
+                Console.WriteLine("Hello, World!");
+                Console.WriteLine(args[0]);
+                Console.WriteLine(args.Length);
+
+            }
+    }
+   
+}
